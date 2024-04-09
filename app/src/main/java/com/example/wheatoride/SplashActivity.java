4802581261302cbf190cf5373395b87abb781f35
Samuel van Vuuren <sam.vanvuuren@my.wheaton.edu>
2024-04-09 16:03:15 -0500

    fixed login

--- app/src/main/java/com/example/wheatoride/SplashActivity.java
+++ app/src/main/java/com/example/wheatoride/SplashActivity.java
@@ -1,9 +1,12 @@
 package com.example.wheatoride;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+
 import com.example.wheatoride.model.UserModel;
 import com.example.wheatoride.utils.AndroidUtil;
 import com.example.wheatoride.utils.FirebaseUtil;
@@ -11,54 +14,51 @@ import com.google.firebase.FirebaseApp;
 import com.google.firebase.appcheck.FirebaseAppCheck;
 import com.google.firebase.appcheck.playintegrity.PlayIntegrityAppCheckProviderFactory;
 
+@SuppressLint("CustomSplashScreen")
 public class SplashActivity extends AppCompatActivity {
-
-    private static final int SPLASH_DELAY_MS = 1000; // Delay in milliseconds
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
-        // Initialize Firebase
         FirebaseApp.initializeApp(/*context=*/ this);
         FirebaseAppCheck firebaseAppCheck = FirebaseAppCheck.getInstance();
         firebaseAppCheck.installAppCheckProviderFactory(
                 PlayIntegrityAppCheckProviderFactory.getInstance());
 
-        // Check if the activity was started from a notification
-        if (getIntent().getExtras() != null) {
+        if(getIntent().getExtras()!=null){
+            //from notification
             String userId = getIntent().getExtras().getString("userId");
             assert userId != null;
             FirebaseUtil.allUserCollectionReference().document(userId).get()
                     .addOnCompleteListener(task -> {
-                        if (task.isSuccessful()) {
+                        if(task.isSuccessful()){
                             UserModel model = task.getResult().toObject(UserModel.class);
 
-                            // Start the main activity
-                            Intent mainIntent = new Intent(this, MainActivity.class);
+                            Intent mainIntent = new Intent(this,MainActivity.class);
                             mainIntent.setFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
                             startActivity(mainIntent);
 
-                            // Start the chat activity
                             Intent intent = new Intent(this, ChatActivity.class);
                             assert model != null;
-                            AndroidUtil.passUserModelAsIntent(intent, model);
+                            AndroidUtil.passUserModelAsIntent(intent,model);
                             intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                             startActivity(intent);
                             finish();
                         }
                     });
-        } else {
-            // Delayed start for non-notification cases
+
+
+        }else{
             new Handler().postDelayed(() -> {
-                if (FirebaseUtil.isLoggedIn()) {
-                    startActivity(new Intent(SplashActivity.this, MainActivity.class));
-                } else {
-                    startActivity(new Intent(SplashActivity.this, LoginGoogleActivity.class));
+                if(FirebaseUtil.isLoggedIn()){
+                    startActivity(new Intent(SplashActivity.this,MainActivity.class));
+                }else{
+                    startActivity(new Intent(SplashActivity.this,LoginGoogleActivity.class));
                 }
                 finish();
-            }, SPLASH_DELAY_MS);
+            },1000);
         }
     }
 }
