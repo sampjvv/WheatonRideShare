973196e0afa64bcf74a41192725e02b8c1b1ee96
Samuel van Vuuren <sam.vanvuuren@my.wheaton.edu>
2024-04-09 20:33:08 -0500

    distinguish between new users and existing users. load existing data for existing users, including desc.

--- app/src/main/java/com/example/wheatoride/LoginGoogleActivity.java
+++ app/src/main/java/com/example/wheatoride/LoginGoogleActivity.java
@@ -2,13 +2,11 @@ package com.example.wheatoride;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
-import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -19,16 +17,15 @@ import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.api.ApiException;
-import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.Timestamp;
 import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.GoogleAuthProvider;
-import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 
-import java.util.HashMap;
 import java.util.Objects;
 
 public class LoginGoogleActivity extends AppCompatActivity {
@@ -37,7 +34,6 @@ public class LoginGoogleActivity extends AppCompatActivity {
     ProgressBar progressBar;
 
     FirebaseAuth auth;
-    FirebaseDatabase database;
     GoogleSignInClient mGoogleSignInClient;
 
     UserModel userModel;
@@ -53,7 +49,6 @@ public class LoginGoogleActivity extends AppCompatActivity {
         progressBar = findViewById(R.id.login_progress_bar);
 
         auth = FirebaseAuth.getInstance();
-        database = FirebaseDatabase.getInstance();
 
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
                 .requestIdToken(getString(R.string.default_web_client_id))
@@ -64,82 +59,66 @@ public class LoginGoogleActivity extends AppCompatActivity {
         progressBar.setVisibility(View.GONE);
 
         googleBtn.setOnClickListener(view -> {
-            googleSignIn();
             progressBar.setVisibility(View.VISIBLE);
+            googleSignIn();
         });
-
     }
 
     private void googleSignIn() {
         Intent intent = mGoogleSignInClient.getSignInIntent();
-        //noinspection deprecation
         startActivityForResult(intent, RC_SIGN_IN);
-
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-
         if (requestCode == RC_SIGN_IN){
-
             Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
-
             try {
                 GoogleSignInAccount account = task.getResult(ApiException.class);
-                progressBar.setVisibility(View.GONE);
                 firebaseAuth(account.getIdToken());
             } catch (Exception e){
                 progressBar.setVisibility(View.GONE);
                 Toast.makeText(this, e.getMessage(), Toast.LENGTH_SHORT).show();
             }
-
-
-
         }
-
     }
-    //line 93
+
     private void firebaseAuth(String idToken) {
-
         AuthCredential credential = GoogleAuthProvider.getCredential(idToken, null);
-        auth.signInWithCredential(credential)
-                .addOnCompleteListener(task -> {
+        auth.signInWithCredential(credential).addOnCompleteListener(task -> {
+            if (task.isSuccessful()) {
+                FirebaseUser user = auth.getCurrentUser();
+                assert user != null;
+                String userId = user.getUid();
 
-                    if (task.isSuccessful()) {
+                DocumentReference docRef = FirebaseUtil.currentUserDetails();
+                docRef.get().addOnCompleteListener(task1 -> {
+                    if (task1.isSuccessful()) {
+                        DocumentSnapshot document = task1.getResult();
+                        if (document.exists()) {
+                            userModel = document.toObject(UserModel.class);
+                        } else {
+                            userModel = new UserModel(user.getEmail(), user.getDisplayName(), Objects.requireNonNull(user.getPhotoUrl()).toString(), Timestamp.now(), userId);
+                            docRef.set(userModel);
+                        }
 
-                        FirebaseUser user = auth.getCurrentUser();
+                        progressBar.setVisibility(View.GONE);
+                        Toast.makeText(LoginGoogleActivity.this, "login successful", Toast.LENGTH_SHORT).show();
 
-                        HashMap<String, Object> map = new HashMap<>();
-                        assert user != null;
-
-                        map.put("id", user.getUid());
-                        map.put("profile", Objects.requireNonNull(user.getPhotoUrl()).toString());
-
-                        userModel = new UserModel(user.getEmail(), user.getDisplayName(), user.getPhotoUrl().toString(), Timestamp.now(), FirebaseUtil.currentUserId());
-
-                        database.getReference().child("users").child(user.getUid()).setValue(map);
-                        FirebaseUtil.currentUserDetails().set(userModel).addOnCompleteListener(new OnCompleteListener<Void>() {
-                            @Override
-                            public void onComplete(@NonNull Task<Void> task) {
-
-                                if (task.isSuccessful()) {
-
-                                    Toast.makeText(LoginGoogleActivity.this, "login successful", Toast.LENGTH_SHORT).show();
-
-                                    Intent intent = new Intent(LoginGoogleActivity.this, MainActivity.class);
-                                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-                                    startActivity(intent);
-                                }
-                            }
-                        });
-
+                        Intent intent = new Intent(LoginGoogleActivity.this, MainActivity.class);
+                        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                        startActivity(intent);
                     } else {
                         Toast.makeText(LoginGoogleActivity.this, "something went wrong", Toast.LENGTH_SHORT).show();
+                        progressBar.setVisibility(View.GONE);
                     }
-
                 });
-
+            } else {
+                Toast.makeText(LoginGoogleActivity.this, "something went wrong", Toast.LENGTH_SHORT).show();
+                progressBar.setVisibility(View.GONE);
+            }
+        });
     }
 }
